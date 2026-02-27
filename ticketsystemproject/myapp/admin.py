f72d9b1e5ed1b73eafc11ce0b649ec4637413c9f
thureamakarani25-svg <thureamakarani25@gmail.com>
2026-02-27 05:45:56 -0800

    Re-add all files

--- ticketsystemproject/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Route, Bus, Schedule, Booking
-
-# Register your models here.
-admin.site.register(Route)
-admin.site.register(Bus)
-admin.site.register(Schedule)
-admin.site.register(Booking) 
-
